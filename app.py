f7a46d17305a31732e783169dd575a8ea4bf9f02
Nikos <nicktheodo1@gmail.com>
2026-04-29 22:05:35 +0300

    fix flask run

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ def home():
 def recipe(name):
     return render_template("recipe.html", recipe=recipes[name])
 
-app.run(debug=True)
+import os 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
